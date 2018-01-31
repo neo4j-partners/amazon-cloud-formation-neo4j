f486c0511372c6972cf7f2e21d08508d0c14c03c
Moxious <moxious@oldhat.org>
2018-01-31 17:02:35 -0500

    simple stress test for generating lots of writes

---
+++ stress.js
@@ -0,0 +1,49 @@
+/*
+ * Quick stress testing script to apply lots of concurrent writes to the cluster.
+ */
+const neo4j = require('neo4j-driver').v1;
+const Promise = require('bluebird');
+
+const concurrency = { concurrency: 100 };
+const ips = [
+ '35.199.177.201',
+ '35.203.178.57',
+ '35.227.186.48',
+];
+
+const url = 'bolt+routing://' + ips[1] + ':7687';
+console.log('Connecting to ', url);
+
+const driver = neo4j.driver(url,
+  neo4j.auth.basic(process.env.NEO4J_USER,
+    process.env.NEO4J_PASSWORD) );
+
+const session = driver.session();
+
+let completed = 0;
+
+const checkpoint = data => {
+   completed++;
+   if(completed % 50 === 0) {
+     console.log('Completed: ', completed);
+   }
+   return data;
+};
+
+const runQuery = () => {
+  const id = Math.floor(Math.random() * 2000 % 1000);
+  const blizz = Math.floor(Math.random() * 1000 % 1000) + 1000;
+  const r = Math.floor(Math.random() * 5 % 5);
+  return session.run('MERGE (n:Node { id: $id }) MERGE (p:Node { id: $blizz }) MERGE (n)-[:link {r: $r}]->(p) RETURN $blizz;', { r, id, blizz })
+     .then(checkpoint);
+};
+
+// Pre-run this prior to script: FOREACH (id IN range(0,1000) | CREATE (:Node {id:id}));
+let numbers = 8000;
+const arr = Array.apply(null, { length: numbers }).map(Number.call, Number);
+
+return Promise.map(arr, item => runQuery(), concurrency)
+  .then(results => console.log('All done!'))
+  .catch(err => console.error(err))
+  .finally(() => driver.close());
+

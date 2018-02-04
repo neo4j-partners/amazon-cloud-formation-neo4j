5d23f879e02c5860838a53ed30da22276adb51f8
Moxious <moxious@oldhat.org>
2018-02-04 15:43:02 -0500

    wait for live and password change post startup

--- post-cluster-startup.sh
+++ post-cluster-startup.sh
@@ -11,3 +11,23 @@
 /usr/bin/neo4j-admin unbind
 /bin/rm -rf /var/lib/neo4j/data/databases/graph.db/
 /bin/systemctl start neo4j.service
+
+while true; do
+    # Loop waiting for neo4j service to start.
+    if curl -s -I http://localhost:7474 | grep "200 OK"; then
+        echo "Neo4j is up; changing default password"
+        # Tmp testing.
+        adminPassword=testdrive
+
+        # Change default password.
+        curl -v -H "Content-Type: application/json" \
+                -XPOST -d '{"password":"admin"}' \
+                -u neo4j:neo4j \
+                http://localhost:7474/user/neo4j/password
+        echo "Done"
+        break
+    fi
+
+    echo "Waiting for neo4j to come up"
+    sleep 1
+done

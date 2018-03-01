791ae8f4fa76376906c398036c25c490f3d8a8aa
Moxious <moxious@oldhat.org>
2018-03-01 13:49:43 -0500

    tweak tweak tweak make amazon happy

--- aws/generate.py
+++ aws/generate.py
@@ -1,6 +1,16 @@
 from jinja2 import Environment, Template, BaseLoader, TemplateNotFound
 from os.path import join, exists, getmtime 
 
+# Custom filters to make generating templates less painful.
+
+def appendStack(input):
+      return """{
+            "Fn::Join": [
+                  "-",
+                  ["%s", { "Ref": "AWS::StackName" }]
+            ]
+      }""" % input
+
 class FileLoader(BaseLoader):
    def __init__(self, path):
       self.path = path
@@ -15,5 +25,8 @@ class FileLoader(BaseLoader):
       return source, path, lambda: mtime == getmtime(path)
 
 env = Environment(loader=FileLoader('./'))
+
+env.filters['appendStack'] = appendStack
+
 template = env.get_template('deploy.jinja')
 print(template.render())

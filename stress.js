78d35e9a644a77b6fe1ad0011bed0f55a3178e1d
Moxious <moxious@oldhat.org>
2018-03-07 07:25:19 -0500

    local tests

--- stress.js
+++ stress.js
@@ -6,12 +6,10 @@ const Promise = require('bluebird');
 
 const concurrency = { concurrency: 100 };
 const ips = [
-  '35.196.168.249',
-  '35.196.95.129',
-  '35.196.54.155',
+   'localhost'
 ];
 
-const url = 'bolt+routing://' + ips[0] + ':7687';
+const url = 'bolt://' + ips[0] + ':7687';
 console.log('Connecting to ', url);
 
 const driver = neo4j.driver(url,
@@ -31,15 +29,15 @@ const checkpoint = data => {
 };
 
 const runQuery = () => {
-  const id = Math.floor(Math.random() * 2000 % 1000);
-  const blizz = Math.floor(Math.random() * 1000 % 1000) + 1000;
+  const id = Math.floor(Math.random() * 200000 % 100000);
+  const blizz = Math.floor(Math.random() * 100000 % 10000) + 100000;
   const r = Math.floor(Math.random() * 5 % 5);
   return session.run('MERGE (n:Node { id: $id }) MERGE (p:Node { id: $blizz }) MERGE (n)-[:link {r: $r}]->(p) RETURN $blizz;', { r, id, blizz })
      .then(checkpoint);
 };
 
 // Pre-run this prior to script: FOREACH (id IN range(0,1000) | CREATE (:Node {id:id}));
-let numbers = 8000;
+let numbers = 80000;
 const arr = Array.apply(null, { length: numbers }).map(Number.call, Number);
 
 return Promise.map(arr, item => runQuery(), concurrency)

9a7ead426ea27c9a573e9a9b7bcd326ca25f9c84
Moxious <moxious@oldhat.org>
2018-07-05 12:20:33 -0400

    test refactoring

--- test/test_cluster.sh
+++ test/test_cluster.sh
@@ -31,92 +31,10 @@ echo "AUTH $auth"
 echo "CORES $CORES"
 echo "RRs $READ_REPLICAS"
 
-# When test resources are deployed cluster hasn't had a chance to form yet.
-# This polls in a loop waiting for cluster to become available, and gives up/fails
-# tests if it doesn't work within attempts.
-attempt=0
-attempts=100
+# Include testing utilities.
+source ./test_common.sh
 
-while true; do
-    attempt=$[$attempt + 1]
-    curl --insecure -s -I $endpoint/ | grep "200 OK"
-    if [ $? -eq 0 ] ; then
-    echo "✔️ Neo4j is up at attempt $attempt"
-    break
-    fi
-
-    if [ $attempt -ge "$attempts" ]; then
-    echo "❌ REST API seems not to be coming up, giving up after $attempts attempts"
-    exit 1
-    fi
-
-    echo "Sleeping; not up yet after $attempt attempts"
-    sleep 5
-done
-
-# Pass index ID to get hostname for that pod.
-function core_hostname {
-
-
-    echo "{{ .Release.Name }}-neo4j-core-$1.{{ .Release.Name }}-neo4j.{{ .Release.Namespace }}.svc.cluster.local"
-}
-
-function replica_hostname {
-    echo "{{ .Release.Name }}-replica-$1.{{ .Release.Name }}-readreplica.{{ .Release.Namespace }}.svc.cluster.local"
-}
-
-test_index=0
-
-function succeed {
-    echo "✔️  Test $test_index: $1"
-    test_index=$[$test_index + 1]
-}
-
-function fail {
-    echo "❌ Test $test_index: $1"
-    echo "Additional information: " "$2"
-    exit 1
-}
-
-function cypher {
-    # Use routing driver by default, send query wherever.
-    DEFAULT_ENDPOINT="bolt+routing://$host:7687"
-
-    # If caller specified, use a specific endpoint to route a query to just one node.
-    ENDPOINT=${2:-$DEFAULT_ENDPOINT}
-
-    echo "$1" | cypher-shell --encryption true -u "$NEO4J_USERNAME" -a "$ENDPOINT" -p "$NEO4J_SECRETS_PASSWORD"
-}
-
-function get_bolt_endpoints_for_core {
-    # Cypher query to find all cluster nodes with role $1 and return their bolt addresses
-    query="call dbms.cluster.overview() yield role, addresses where role='LEADER' or role='FOLLOWER' WITH addresses UNWIND addresses as address WITH address where address =~ 'bolt:.*' return address ORDER BY address asc;"
-    cypher "$query" | grep bolt | sed 's/"//g'
-}
-
-function get_bolt_endpoints_for_rr {
-    # Cypher query to find all cluster nodes with role $1 and return their bolt addresses
-    query="call dbms.cluster.overview() yield role, addresses where role='READ_REPLICA' WITH addresses UNWIND addresses as address WITH address where address =~ 'bolt:.*' return address ORDER BY address asc;"
-    cypher "$query" | grep bolt | sed 's/"//g'
-}
-
-function runtest {
-    # Use routing driver by default, send query wherever.
-    DEFAULT_ENDPOINT="bolt+routing://$host:7687"
-
-    # If caller specified, use a specific endpoint to route a query to just one node.
-    ENDPOINT=${3:-$DEFAULT_ENDPOINT}
-
-    echo "Running $1 against $ENDPOINT"
-    output=$(cypher "$2" "$3")
-
-    if [ $? -eq 0 ] ; then  
-    succeed "$1"
-    else
-    echo "Last output -- $output"
-    fail "$1" "$output"
-    fi
-}
+wait_for_live "$endpoint"
 
 test="HTTPS is available, port 7443"
 curl --insecure https://$host:7473/
@@ -166,9 +84,11 @@ else
 fi
 
 # Each core is individually up and configured.
+id=0
 for core_endpoint in $(get_bolt_endpoints_for_core); do
     echo "Core endpoint $core_endpoint"
-    test="Core host $CORES -- $core_endpoint is available"
+    id=$[$id + 1]
+    test="Core host $id of $CORES -- $core_endpoint is available"
     runtest "$test" "MATCH (n) RETURN COUNT(n);" "$core_endpoint"
 
     test="Core host $CORES -- $core_endpoint has APOC installed correctly"
@@ -181,8 +101,10 @@ echo "Sleeping a few seconds to permit replication"
 sleep 5
 
 # Check each core, count the canary writes. They should all agree.
+id=0
 for core_endpoint in $(get_bolt_endpoints_for_core); do
-    test="Core endpoint $core_endpoint has the canary write"
+    id=$[$id + 1]
+    test="Core endpoint $id of $CORES $core_endpoint has the canary write"
     result=$(cypher "MATCH (c:Canary) WITH count(c) as x where x = 1 RETURN x;" "$core_endpoint")
     exit_code=$?
     if [ $exit_code -eq 0 ] ; then

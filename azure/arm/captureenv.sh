b42d4d2b84fb87b3413d743de7468f09b61a96ee
Moxious <moxious@oldhat.org>
2018-08-16 15:44:56 -0400

    SIMPLIFY: remove unused files

--- azure/arm/captureenv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "#!/bin/bash" > "$(dirname $0)/re-setup.sh"
-compgen -v | while read VAR; do
-    echo "if [ -z \"\$$VAR\" ]; then" >> "$(dirname $0)/re-setup.sh"
-    declare -xp $VAR >> "$(dirname $0)/re-setup.sh"
-    echo "fi" >> "$(dirname $0)/re-setup.sh"
-done
-echo 'source "$(dirname $0)/setup.sh"' >> "$(dirname $0)/re-setup.sh"

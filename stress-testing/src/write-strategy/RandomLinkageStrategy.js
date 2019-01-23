f72fd6494f57a42246f2b3f721e90817bbd83d65
Moxious <moxious@oldhat.org>
2019-01-23 18:32:16 -0500

    relationship props on random linkages

--- stress-testing/src/write-strategy/RandomLinkageStrategy.js
+++ stress-testing/src/write-strategy/RandomLinkageStrategy.js
@@ -10,17 +10,24 @@ class RandomLinkageStrategy extends Strategy {
     }
 
     run(driver) {
+
         this.lastQuery = `
             MATCH (a) 
             WITH a 
             SKIP $idx1 LIMIT 3 
             MATCH (b) WITH a,b 
             SKIP $idx2 LIMIT 3 
-            CREATE (a)-[r:randomlinkage]->(b)
+            CREATE (a)-[r:randomlinkage {
+                id: $id, created: datetime()
+            }]->(b)
             RETURN count(r)
         `;
         
-        this.lastParams = { idx1: this.randInt(this.n), idx2: this.randInt(this.n) };
+        this.lastParams = { 
+            idx1: this.randInt(this.n), 
+            idx2: this.randInt(this.n),
+            id: uuid.v4(), 
+        };
         const f = (s = driver.session()) => s.writeTransaction(tx => tx.run(this.lastQuery, this.lastParams));
         return this.time(f);
     }

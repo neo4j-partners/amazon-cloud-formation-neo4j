17f8f32a3144ec9c102027a064ecbb0695ba87ec
Moxious <moxious@oldhat.org>
2019-02-05 20:19:14 -0500

    stack settings and adjustable disk types

--- test/benchmarking/providers/gcp/create.sh
+++ test/benchmarking/providers/gcp/create.sh
@@ -4,17 +4,25 @@ export RUN_ID=$(head -c 1024 /dev/urandom | md5)
 export NAME=neo4j-testdeploy-$(head -c 3 /dev/urandom | md5 | head -c 5)
 PROJECT=testbed-187316
 MACHINE=n1-standard-4
+DISK_TYPE=pd-standard
+# DISK_TYPE=pd-ssd
 
 OUTPUT=$(gcloud deployment-manager deployments create $NAME \
     --project $PROJECT \
     --template ../../../../gcloud/solutions/causal-cluster/neo4j-causal-cluster.jinja \
-    --properties "clusterNodes:'3',readReplicas:'0',machineType:'$MACHINE'")
+    --properties "clusterNodes:'3',readReplicas:'0',bootDiskType:'$DISK_TYPE',machineType:'$MACHINE'")
 echo $OUTPUT
+
+echo STACK_SETTING_CORE_NODES=3
+echo STACK_SETTING_READ_REPLICAS=0
+echo STACK_SETTING_MACHINE_TYPE=$MACHINE
+echo STACK_SETTING_GCP_PROJECT=$PROJECT
+echo STACK_SETTING_DISK_TYPE=$DISK_TYPE
 
 PASSWORD=$(echo $OUTPUT | perl -ne 'm/password\s+([^\s]+)/; print $1;')
 IP=$(echo $OUTPUT | perl -ne 'm/vm1URL\s+https:\/\/([^\s]+):/; print $1; ')
 
-echo NEO4J_IP=$IP
+echo NEO4J_URI=bolt+routing://$IP
 echo NEO4J_PASSWORD=$PASSWORD
 echo STACK_NAME=$NAME
 echo RUN_ID=$RUN_ID

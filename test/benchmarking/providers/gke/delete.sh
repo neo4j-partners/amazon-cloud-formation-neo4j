32dbb813c9d53f91e99bfc2888ee78468bc3d154
Moxious <moxious@oldhat.org>
2019-02-07 08:23:09 -0500

    rename creates/deletes for GKE provider

---
+++ test/benchmarking/providers/gke/delete.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -z $1 ] ; then
+   echo "Missing argument"
+   exit 1
+fi
+
+echo "Deleting stack $1"
+
+echo "Killing port forwarding"
+kill $(ps -A | grep -m1 "kubectl port-forward" | awk '{print $1}')
+
+# Delete the stack by tearing down all of the resources in the YAML we created
+# as part of the create step.
+kubectl delete -f "$1.yaml"
+KUBECTL_EXIT=$?
+rm -f "$1.yaml"
+
+echo "Dumping kubectl forward logs for posterity..."
+echo "========================"
+cat kubectl-$1.log
+echo "========================"
+
+rm -f "kubectl-$1.log" nohup.out
+
+exit $KUBECTL_EXIT

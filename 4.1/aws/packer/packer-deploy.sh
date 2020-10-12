df461ed380b84808cfb7f034ad95af5dc895a02d
Bledi Feshti <bledifeshti@neotechnology.com>
2020-10-12 14:12:04 +0200

    Added conditional parameters to packer-deploy

--- 4.1/aws/packer/packer-deploy.sh
+++ 4.1/aws/packer/packer-deploy.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+if [[ $ENVIRONMENT == "PROD" ]]; then
+export VERSION=4.1.3
+packer build \
+    -var "neo4j_edition=enterprise" \
+    -var "neo4j_version=1:4.1.3" \
+    packer-template.json
 
+#packer build \
+#    -var "neo4j_edition=community" \
+#    -var "neo4j_version=1:4.1.3" \
+#    packer-template.json
+
+elif [[ $ENVIRONMENT == "TEST" ]]; then
 export VERSION=4.1.3
 packer build \
     -var "neo4j_edition=enterprise" \
@@ -10,14 +22,6 @@ packer build \
     -var "neo4j_edition=community" \
     -var "neo4j_version=1:4.1.3" \
     packer-template-test.json
-
-#export PROFILE=govcloud
-#export AWS_PROFILE=govcloud
-#packer build \
-#    -var "neo4j_edition=enterprise" \
-#    -var "neo4j_version=1:4.1.3" \
-#    -var "region=us-gov-east-1" \
-#    -var "destination_regions=us-gov-west-1" \
-#    -var "instance_type=t3.micro" \
-#    -var "base_owner=513442679011" \
-#    packer-template-test.json
+else
+   echo "Parameter: $ENVIRONMENT is not valid"
+fi

3367652127d83033f545fb20940a937e07ab8d92
Harshit Singhvi <harshit.singhvi@neotechnology.com>
2022-06-21 13:20:37 +0100

    Adding ssr changes

--- marketplace/deploy.sh
+++ marketplace/deploy.sh
@@ -6,9 +6,13 @@ TEMPLATE_BODY="file://neo4j.template.yaml"
 REGION=`aws configure get region`
 
 Password="foo123"
-KeyName="neo4j-${REGION}"
+#KeyName="neo4j-${REGION}"
+KeyName="harshit"
 NodeCount="3"
 SSHCIDR="0.0.0.0/0"
+VPCCIDR="10.0.0.0/16"
+SubnetCIDR="10.0.1.0/24"
+SelectSSR=true
 
 aws cloudformation create-stack \
 --capabilities CAPABILITY_IAM \
@@ -19,4 +23,9 @@ aws cloudformation create-stack \
 ParameterKey=Password,ParameterValue=${Password} \
 ParameterKey=KeyName,ParameterValue=${KeyName} \
 ParameterKey=NodeCount,ParameterValue=${NodeCount} \
-ParameterKey=SSHCIDR,ParameterValue=${SSHCIDR}
+ParameterKey=SSHCIDR,ParameterValue=${SSHCIDR} \
+ParameterKey=VPCCIDR,ParameterValue=${VPCCIDR} \
+ParameterKey=SubnetCIDR,ParameterValue=${SubnetCIDR} \
+ParameterKey=InstallGraphDataScience,ParameterValue=False \
+ParameterKey=InstallBloom,ParameterValue=False \
+ParameterKey=SelectSSR,ParameterValue=${SelectSSR}

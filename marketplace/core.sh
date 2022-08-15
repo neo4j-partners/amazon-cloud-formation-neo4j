2e97fc7c8f4e7769fccf773e31b9bb4f234138f2
Harshit Singhvi <harshit.singhvi@neotechnology.com>
2022-08-15 14:39:45 +0100

    Fixing echo statements

--- marketplace/core.sh
+++ marketplace/core.sh
@@ -12,7 +12,7 @@ stackName=${10}
 region=${11}
 loadBalancerDNSName=${12}
 
-echo Adding neo4j yum repo...
+echo "Adding neo4j yum repo..."
 rpm --import https://debian.neo4j.com/neotechnology.gpg.key
 echo "
 [neo4j]
@@ -21,22 +21,22 @@ baseurl=http://yum.neo4j.com/stable
 enabled=1
 gpgcheck=1" > /etc/yum.repos.d/neo4j.repo
 
-echo Installing Graph Database...
+echo "Installing Graph Database..."
 export NEO4J_ACCEPT_LICENSE_AGREEMENT=yes
 yum -y install neo4j-enterprise-${graphDatabaseVersion}
 yum update -y aws-cfn-bootstrap
 systemctl enable neo4j
 
-echo Installing APOC...
+echo "Installing APOC..."
 mv /var/lib/neo4j/labs/apoc-*-core.jar /var/lib/neo4j/plugins
 
 echo Configuring extensions and security in neo4j.conf...
 sed -i s~#dbms.unmanaged_extension_classes=org.neo4j.examples.server.unmanaged=/examples/unmanaged~dbms.unmanaged_extension_classes=com.neo4j.bloom.server=/bloom,semantics.extension=/rdf~g /etc/neo4j/neo4j.conf
 sed -i s/#dbms.security.procedures.unrestricted=my.extensions.example,my.procedures.*/dbms.security.procedures.unrestricted=gds.*,apoc.*,bloom.*/g /etc/neo4j/neo4j.conf
-echo dbms.security.http_auth_allowlist=/,/browser.*,/bloom.* >> /etc/neo4j/neo4j.conf
-echo dbms.security.procedures.allowlist=apoc.*,gds.*,bloom.* >> /etc/neo4j/neo4j.conf
+echo "dbms.security.http_auth_allowlist=/,/browser.*,/bloom.*" >> /etc/neo4j/neo4j.conf
+echo "dbms.security.procedures.allowlist=apoc.*,gds.*,bloom.*" >> /etc/neo4j/neo4j.conf
 
-echo Configuring network in neo4j.conf...
+echo "Configuring network in neo4j.conf..."
 sed -i 's/#dbms.default_listen_address=0.0.0.0/dbms.default_listen_address=0.0.0.0/g' /etc/neo4j/neo4j.conf
 
 privateIP=$(hostname -i | awk {'print $NF'})
@@ -52,22 +52,22 @@ function set_cluster_configs {
    sed -i s/#dbms.routing.enabled=false/dbms.routing.enabled=true/g /etc/neo4j/neo4j.conf
    sed -i s/#dbms.routing.advertised_address=:7688/dbms.routing.advertised_address=${privateIP}:7688/g /etc/neo4j/neo4j.conf
    sed -i s/#dbms.routing.listen_address=0.0.0.0:7688/dbms.routing.listen_address=${privateIP}:7688/g /etc/neo4j/neo4j.conf
-   echo dbms.routing.default_router=SERVER >> /etc/neo4j/neo4j.conf
+   echo "dbms.routing.default_router=SERVER" >> /etc/neo4j/neo4j.conf
 }
 
 if [[ $nodeCount == 1 ]]; then
-  echo Running on a single node.
+  echo "Running on a single node."
 
   if [[ $readReplicaCount == 0 ]]; then
      sed -i s/#dbms.default_advertised_address=localhost/dbms.default_advertised_address=${loadBalancerDNSName}/g /etc/neo4j/neo4j.conf
   else
      sed -i s/#dbms.mode=CORE/dbms.mode=SINGLE/g /etc/neo4j/neo4j.conf
-     echo dbms.clustering.enable=true >> /etc/neo4j/neo4j.conf
+     echo "dbms.clustering.enable=true" >> /etc/neo4j/neo4j.conf
      set_cluster_configs
   fi
 
 else
-  echo Running on multiple nodes.  Configuring membership in neo4j.conf...
+  echo "Running on multiple nodes.  Configuring membership in neo4j.conf..."
   region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/.$//')
   instanceId=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
   stackName=$(aws cloudformation describe-stack-resources --physical-resource-id $instanceId --query 'StackResources[0].StackName' --output text --region $region)
@@ -80,30 +80,30 @@ else
 fi
 
 if [[ $installGraphDataScience == True && $nodeCount == 1 ]]; then
-  echo Installing Graph Data Science...
+  echo "Installing Graph Data Science..."
   cp /var/lib/neo4j/products/neo4j-graph-data-science-*.jar /var/lib/neo4j/plugins
 fi
 
 if [[ $graphDataScienceLicenseKey != None ]]; then
-  echo Writing GDS license key...
+  echo "Writing GDS license key..."
   mkdir -p /etc/neo4j/licenses
   echo $graphDataScienceLicenseKey > /etc/neo4j/licenses/neo4j-gds.license
   sed -i '$a gds.enterprise.license_file=/etc/neo4j/licenses/neo4j-gds.license' /etc/neo4j/neo4j.conf
 fi
 
 if [[ $installBloom == True ]]; then
-  echo Installing Bloom...
+  echo "Installing Bloom..."
   cp /var/lib/neo4j/products/bloom-plugin-*.jar /var/lib/neo4j/plugins
 fi
 
 if [[ $bloomLicenseKey != None ]]; then
-  echo Writing Bloom license key...
+  echo "Writing Bloom license key..."
   mkdir -p /etc/neo4j/licenses
   echo $bloomLicenseKey > /etc/neo4j/licenses/neo4j-bloom.license
   sed -i '$a neo4j.bloom.license_file=/etc/neo4j/licenses/neo4j-bloom.license' /etc/neo4j/neo4j.conf
 fi
 
-echo Starting Neo4j...
+echo "Starting Neo4j..."
 service neo4j start
 neo4j-admin set-initial-password ${password}
 /opt/aws/bin/cfn-signal -e $? --stack ${stackName} --resource Neo4jAutoScalingGroup --region ${region}
--- marketplace/readreplica.sh
+++ marketplace/readreplica.sh
@@ -6,7 +6,7 @@ readReplicaCount=$4
 stackName=$5
 region=$6
 
-echo Adding neo4j yum repo...
+echo "Adding neo4j yum repo..."
 rpm --import https://debian.neo4j.com/neotechnology.gpg.key
 echo "
 [neo4j]
@@ -15,22 +15,22 @@ baseurl=http://yum.neo4j.com/stable
 enabled=1
 gpgcheck=1" > /etc/yum.repos.d/neo4j.repo
 
-echo Installing Graph Database...
+echo "Installing Graph Database..."
 export NEO4J_ACCEPT_LICENSE_AGREEMENT=yes
 yum -y install neo4j-enterprise-${graphDatabaseVersion}
 yum update -y aws-cfn-bootstrap
 systemctl enable neo4j
 
-echo Installing APOC...
+echo "Installing APOC..."
 mv /var/lib/neo4j/labs/apoc-*-core.jar /var/lib/neo4j/plugins
 
-echo Configuring extensions and security in neo4j.conf...
+echo "Configuring extensions and security in neo4j.conf..."
 sed -i s~#dbms.unmanaged_extension_classes=org.neo4j.examples.server.unmanaged=/examples/unmanaged~dbms.unmanaged_extension_classes=com.neo4j.bloom.server=/bloom,semantics.extension=/rdf~g /etc/neo4j/neo4j.conf
 sed -i s/#dbms.security.procedures.unrestricted=my.extensions.example,my.procedures.*/dbms.security.procedures.unrestricted=gds.*,apoc.*,bloom.*/g /etc/neo4j/neo4j.conf
-echo dbms.security.http_auth_allowlist=/,/browser.*,/bloom.* >> /etc/neo4j/neo4j.conf
-echo dbms.security.procedures.allowlist=apoc.*,gds.*,bloom.* >> /etc/neo4j/neo4j.conf
+echo "dbms.security.http_auth_allowlist=/,/browser.*,/bloom.*" >> /etc/neo4j/neo4j.conf
+echo "dbms.security.procedures.allowlist=apoc.*,gds.*,bloom.*" >> /etc/neo4j/neo4j.conf
 
-echo Configuring network in neo4j.conf...
+echo "Configuring network in neo4j.conf..."
 sed -i 's/#dbms.default_listen_address=0.0.0.0/dbms.default_listen_address=0.0.0.0/g' /etc/neo4j/neo4j.conf
 
 privateIP=$(hostname -i | awk {'print $NF'})
@@ -48,9 +48,9 @@ sed -i s/#causal_clustering.initial_discovery_members=localhost:5000,localhost:5
 sed -i s/#dbms.routing.enabled=false/dbms.routing.enabled=true/g /etc/neo4j/neo4j.conf
 sed -i s/#dbms.routing.advertised_address=:7688/dbms.routing.advertised_address=${privateIP}:7688/g /etc/neo4j/neo4j.conf
 sed -i s/#dbms.routing.listen_address=0.0.0.0:7688/dbms.routing.listen_address=${privateIP}:7688/g /etc/neo4j/neo4j.conf
-echo dbms.routing.default_router=SERVER >> /etc/neo4j/neo4j.conf
+echo "dbms.routing.default_router=SERVER" >> /etc/neo4j/neo4j.conf
 
-echo Starting Neo4j...
+echo "Starting Neo4j..."
 service neo4j start
 neo4j-admin set-initial-password ${password}
 /opt/aws/bin/cfn-signal -e $? --stack ${stackName} --resource Neo4jReadReplicaAutoScalingGroup --region ${region}

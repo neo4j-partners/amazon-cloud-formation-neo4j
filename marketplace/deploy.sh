ebe89399e49a907485f03e2bb216476a34e4ce55
Ollie Hughes <oliver.hughes@neotechnology.com>
2022-09-29 14:00:30 +0100

    Inline core.sh and readreplica.sh into template
    Use functions to organise script
    Wait for webserver before signalling that instance ready
    Reduce load balancer health check period from 30 to 10 seconds to improve the time a user has to wait for the browser to be ready after deploying the stack

--- marketplace/deploy.sh
+++ marketplace/deploy.sh
@@ -6,17 +6,22 @@ TEMPLATE_BODY="file://neo4j.template.yaml"
 REGION=`aws configure get region`
 
 Password="foo123"
-NodeCount="3"
+CoreInstanceCount="3"
+ReadReplicaCount="2"
 SSHCIDR="0.0.0.0/0"
+GraphDatabaseVersion=4.4.11
 
 aws cloudformation create-stack \
 --capabilities CAPABILITY_IAM \
 --stack-name ${STACK_NAME} \
 --template-body ${TEMPLATE_BODY} \
 --region ${REGION} \
+--disable-rollback \
 --parameters \
 ParameterKey=Password,ParameterValue=${Password} \
-ParameterKey=NodeCount,ParameterValue=${NodeCount} \
+ParameterKey=GraphDatabaseVersion,ParameterValue=${GraphDatabaseVersion} \
+ParameterKey=CoreInstanceCount,ParameterValue=${CoreInstanceCount} \
 ParameterKey=SSHCIDR,ParameterValue=${SSHCIDR} \
 ParameterKey=InstallGraphDataScience,ParameterValue=False \
-ParameterKey=InstallBloom,ParameterValue=False
+ParameterKey=InstallBloom,ParameterValue=False \
+ParameterKey=ReadReplicaCount,ParameterValue=${ReadReplicaCount}

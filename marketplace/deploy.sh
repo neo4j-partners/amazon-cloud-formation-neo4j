32e2da2427820f8ae5eca077e674e166bb989a15
Ollie Hughes <oliver.hughes@neotechnology.com>
2022-09-29 14:11:48 +0100

    Update scripts to Neo4j version 5
    Inline core.sh and readreplica.sh into template
    Use functions to organise script
    Wait for webserver before signalling that instance ready
    Reduce load balancer health check period from 30 to 10 seconds to improve the time a user has to wait for the browser to be ready after deploying the stack

--- marketplace/deploy.sh
+++ marketplace/deploy.sh
@@ -6,17 +6,26 @@ TEMPLATE_BODY="file://neo4j.template.yaml"
 REGION=`aws configure get region`
 
 Password="foo123"
-NodeCount="3"
+CoreInstanceCount="3"
+ReadReplicaCount="2"
 SSHCIDR="0.0.0.0/0"
+GraphDatabaseVersion=5.0.0
+S3AccessKeyId=$2
+S3SecretAccessKey=$3
 
 aws cloudformation create-stack \
 --capabilities CAPABILITY_IAM \
 --stack-name ${STACK_NAME} \
 --template-body ${TEMPLATE_BODY} \
 --region ${REGION} \
+--disable-rollback \
 --parameters \
 ParameterKey=Password,ParameterValue=${Password} \
-ParameterKey=NodeCount,ParameterValue=${NodeCount} \
+ParameterKey=S3AccessKeyId,ParameterValue=${S3AccessKeyId} \
+ParameterKey=S3SecretAccessKey,ParameterValue=${S3SecretAccessKey} \
+ParameterKey=GraphDatabaseVersion,ParameterValue=${GraphDatabaseVersion} \
+ParameterKey=CoreInstanceCount,ParameterValue=${CoreInstanceCount} \
 ParameterKey=SSHCIDR,ParameterValue=${SSHCIDR} \
 ParameterKey=InstallGraphDataScience,ParameterValue=False \
-ParameterKey=InstallBloom,ParameterValue=False
+ParameterKey=InstallBloom,ParameterValue=False \
+ParameterKey=ReadReplicaCount,ParameterValue=${ReadReplicaCount}

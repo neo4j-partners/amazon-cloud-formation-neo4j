e100e73cde164a73b5092104c13b4547f2369b7e
Ed Randall <github@edrandall.uk>
2022-11-04 10:54:55 +0000

    First commit for SSL/NLB

---
+++ custom-templates/neo4j-nlb-with-ssl/deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+STACK_NAME=$1
+TEMPLATE_BODY="file://nlb_with_ssl.yaml"
+REGION=`aws configure get region`
+
+Password="foo123"
+NumberOfServers="6"
+SSHCIDR="0.0.0.0/0"
+GraphDatabaseVersion=5.1.0
+KeyName="edr-us-east-1"
+SSLDomain="neo4j.aws-cloud.uk"
+CertificateName="arn:aws:acm:us-east-1:540622579701:certificate/5b82a00f-791c-4a35-8e09-8d9fd55bf4f6"
+
+aws cloudformation create-stack \
+--capabilities CAPABILITY_IAM \
+--stack-name ${STACK_NAME} \
+--template-body ${TEMPLATE_BODY} \
+--region ${REGION} \
+--disable-rollback \
+--parameters \
+ParameterKey=Password,ParameterValue=${Password} \
+ParameterKey=GraphDatabaseVersion,ParameterValue=${GraphDatabaseVersion} \
+ParameterKey=NumberOfServers,ParameterValue=${NumberOfServers} \
+ParameterKey=SSHCIDR,ParameterValue=${SSHCIDR} \
+ParameterKey=InstallGraphDataScience,ParameterValue=False \
+ParameterKey=InstallBloom,ParameterValue=False
+ParameterKey=SSLDomain,ParameterValue=${SSLDomain} \
+ParameterKey=CertificateName,ParameterValue=${CertificateName}

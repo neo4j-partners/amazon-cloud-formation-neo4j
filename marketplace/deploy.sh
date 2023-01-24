6f441abf02fe686a8c5ddd3cfb141f12f95fcdda
edrandall-dev <ed.randall@neo4j.com>
2023-01-24 15:35:15 +0000

    Updated deploy.sh script

--- marketplace/deploy.sh
+++ marketplace/deploy.sh
@@ -1,23 +1,65 @@
 #!/bin/bash
 
-STACK_NAME=$1
+#
+# Script:  deploy.sh
+# Purpose: This script can be used to deploy the Neo4j CloudFormation template if the aws cli
+#          is correctly installed and configured
+#
 
-TEMPLATE_BODY="file://neo4j.template.yaml"
-REGION=`aws configure get region`
+###################################################################################
 
-Password="foo123"
-NumberOfServers="6"
+# User configurable variables
+Password="foobar123"
+NumberOfServers=1
 SSHCIDR="0.0.0.0/0"
+InstallGraphDataScience="True"
+InstallBloom="False"
+REGION="eu-central-1"
+Password="foobar123"
+graphDataScienceLicenseKey="None"
 
-aws cloudformation create-stack \
+###################################################################################
+
+# Other Variables (changes not normally required)
+AWS=$(basename "$(which aws)") ||  { echo "Please ensure that the AWS cli client is installed." && exit 1; };
+STACK_NAME=$1
+TEMPLATE_BODY="file://neo4j.template.yaml"
+
+###################################################################################
+
+if [ $NumberOfServers == 2 ] || [ $NumberOfServers -gt 10 ] || [ $NumberOfServers -lt 1 ]; then
+  echo "A single instance, or between 3 and 10 instances can be installed."
+  exit 1
+fi
+
+if [ $InstallGraphDataScience == "True" ] && [ $NumberOfServers != 1 ] ; then
+  echo "GDS cannot be installed on a cluster. NumberOfServers must be set to \"1\" if InstallGraphDataScience is set to \"True\"."
+  exit 1
+fi
+
+if [ -z $graphDataScienceLicenseKey ] || [ $graphDataScienceLicenseKey == "None" ] && [ $InstallGraphDataScience == "True" ] ; then
+  echo "The variable graphDataScienceLicenseKey should not be set if InstallGraphDataScience is set to \"False\"."
+  exit 1
+fi
+
+if [ "$#" != 1 ] ; then
+  echo "This script takes a single argument, the desired name of the target cloudformation stack."
+  echo "Usage: { $0 [stack-name] }"
+  exit 1
+fi
+
+###################################################################################
+
+$AWS cloudformation create-stack \
 --capabilities CAPABILITY_IAM \
---stack-name ${STACK_NAME} \
---template-body ${TEMPLATE_BODY} \
---region ${REGION} \
+--stack-name $STACK_NAME \
+--template-body $TEMPLATE_BODY \
+--region $REGION \
 --disable-rollback \
 --parameters \
 ParameterKey=Password,ParameterValue=${Password} \
 ParameterKey=NumberOfServers,ParameterValue=${NumberOfServers} \
 ParameterKey=SSHCIDR,ParameterValue=${SSHCIDR} \
-ParameterKey=InstallGraphDataScience,ParameterValue=False \
-ParameterKey=InstallBloom,ParameterValue=False
+ParameterKey=InstallGraphDataScience,ParameterValue=${InstallGraphDataScience} \
+ParameterKey=GraphDataScienceLicenseKey,ParameterValue=${graphDataScienceLicenseKey} \
+ParameterKey=InstallBloom,ParameterValue=${InstallBloom}
